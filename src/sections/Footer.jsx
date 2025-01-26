6a2daaa18704311f24943151686d1b7524a16356
Elya <elyaj.le@gmail.com>
2025-01-26 08:15:12 -0600

    fine tuning the padding of the footer copy

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { CiStar } from "react-icons/ci";
+import { PiCopyrightLight } from "react-icons/pi";
 
 const Footer = () => {
   return (
@@ -10,13 +11,41 @@ const Footer = () => {
           backgroundColor: "#191B00", // Footer background color
         }}
       >
-        <p className="text-sm font-thin">
-          © {new Date().getFullYear()}{" "}
-          <a href="https://www.elya.dev" className="underline">
-            www.elya.dev
-          </a>{" "}
-          — Designed & built by me, Elya <span style={{ display: "inline-flex", alignItems: "center" }}><CiStar /></span>
-        </p>
+        <p
+          className="text-sm font-thin flex items-center justify-center gap-1"
+          style={{ lineHeight: 1.5 }}
+        >
+        {/* copyright icon */}
+        <span
+          style={{
+            display: "inline-flex",
+            alignItems: "center",
+            marginBottom: "", // fine-tune the alignment
+          }}
+        >
+        <PiCopyrightLight />
+        </span>
+
+        {/* text content */}
+        <span style={{ alignItems: "center", paddingBottom: "1px" }}>
+        {new Date().getFullYear()}{" "}
+        <a href="https://www.elya.dev" className="underline">
+          www.elya.dev
+        </a>{" "}
+          — Designed & built by me, Elya
+        </span>
+
+        {/* star icon */}
+        <span
+          style={{
+            display: "inline-flex",
+            alignItems: "center",
+            marginBottom: "", // fine-tune the alignment
+          }}
+        >
+        <CiStar />
+        </span>
+      </p>
       </footer>
     </div>
   );
